15697f88d4ad153f2f9e10df642d8ac6ab140699
andyyang777 <40268501+andyyang777@users.noreply.github.com>
2020-02-18 01:39:59 -0500

    Create TwoSum_Solu2_HashMap.java

---
+++ Leetcode/TwoSum_Solu2_HashMap.java
@@ -0,0 +1,37 @@
+//Given an array of integers, return indices of the two numbers such that they a
+//dd up to a specific target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// Example: 
+//
+// 
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+// 
+// Related Topics Array Hash Table
+
+
+import java.util.HashMap;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            final Integer value = map.get(nums[i]);
+            if (value != null){
+                return new int[] {value,i};
+            }
+            map.put(target-nums[i],i);
+        }
+        return null;
+
+    }
+}
+//方法2，利用HashMap
+
+//leetcode submit region end(Prohibit modification and deletion)
